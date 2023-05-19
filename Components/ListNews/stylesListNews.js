fe3a61b4d88ed6c2abbcc23e803cb77030daa7f5
cuongtran <cuongtran181299@gmail.com>
2023-05-19 08:46:54 +0700

    stylesListNews

---
+++ Components/ListNews/stylesListNews.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    accountPost: {
+        alignItems: 'center',
+        marginRight: 10,
+    },
+    story: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        borderWidth: 3,
+        borderColor: '#FF8501'
+    },
+    nameAccount: {
+        fontSize: 17,
+        fontWeight: '500',
+        color: '#fff'
+    },
+});
+
+export default styles;
+
+
